662c5479ec0ec1cac985b17541a6243aa874dae5
Felix <felix.boesing@t-online.de>
2021-01-27 15:09:27 +0100

    reduce case

--- src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
@@ -1,5 +1,7 @@
 package com.sparkTutorial.rdd.sumOfNumbers
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 object SumOfNumbersProblem {
 
   def main(args: Array[String]) {
@@ -9,5 +11,18 @@ object SumOfNumbersProblem {
 
        Each row of the input file contains 10 prime numbers separated by spaces.
      */
+
+    val conf = new SparkConf().setAppName("SumOfNumbers").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    val prime_numbers = sc.textFile("in/prime_nums.text")
+    var primes_flatten = prime_numbers.flatMap(line => line.split("\\s+"))
+    primes_flatten = primes_flatten.filter(number => !number.isEmpty)
+    val primes_flatten_number = primes_flatten.map(number => number.toInt)
+    for (number <- primes_flatten_number) println("" + number)
+    val sum_primes = primes_flatten_number.reduce((x, y) => x + y)
+
+    println("Sum of primes: " + sum_primes)
+
   }
 }
